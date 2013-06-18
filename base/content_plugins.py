c480080688c7e306516fbaf401ca1c6b7005d44c
Willaru <willaru@willaru-Inspiron-1525.(none)>
2013-06-18 19:06:17 -0300

    Terminar blog y cambiar aspectos del layout

--- base/content_plugins.py
+++
@@ -1,9 +0,0 @@
-from fluent_contents.extensions import plugin_pool, ContentPlugin
-from base.models import ImageItem
-
-
-@plugin_pool.register
-class ImagePlugin(ContentPlugin):
-    model = ImageItem
-    render_template = "plugins/image_item.html"
-    category = "Simple blocks"
